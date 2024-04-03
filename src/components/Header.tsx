d1a8471e68eb00923a4424208322c9b595c5edf6
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-04-03 17:04:00 +0900

    created header and Navbar with basic styling

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import Navbar from './Navbar';
+import './header.css';
+
+export default function Header() {
+  return (
+    <>
+      <div className="header-div">
+        <h1>Kevin Tim</h1>
+        <Navbar />
+      </div>
+    </>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,82 @@
+import { Link } from 'react-scroll';
+import { useRef, useState } from 'react';
+import './header.css';
+import { Burger, useMantineTheme } from '@mantine/core';
+import useOutsideClick from '../hook/useOutsideClick';
+import { useMediaQuery } from '@mantine/hooks';
+
+export default function Navbar() {
+  const [opened, setOpened] = useState(false);
+  const theme = useMantineTheme();
+  const dropRef = useRef<HTMLDivElement | null>(null);
+  const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+
+  useOutsideClick(dropRef, () => setOpened(false));
+
+  return (
+    <>
+      <div>
+        {isMobile ? (
+          <>
+            <Burger opened={opened} onClick={() => setOpened((o) => !o)} />
+            {opened && (
+              <nav ref={dropRef} className="dropdown">
+                <Link
+                  to="projects"
+                  smooth={true}
+                  duration={1000}
+                  onClick={() => setOpened(false)}
+                >
+                  Projects
+                </Link>
+                <Link
+                  to="contact"
+                  smooth={true}
+                  duration={1000}
+                  onClick={() => setOpened(false)}
+                >
+                  Contact
+                </Link>
+                <Link
+                  to="about"
+                  smooth={true}
+                  duration={1000}
+                  onClick={() => setOpened(false)}
+                >
+                  About
+                </Link>
+              </nav>
+            )}
+          </>
+        ) : (
+          <nav className="desk-nav">
+            <Link
+              to="about"
+              smooth={true}
+              duration={1000}
+              onClick={() => setOpened(false)}
+            >
+              Projects
+            </Link>
+            <Link
+              to="about"
+              smooth={true}
+              duration={1000}
+              onClick={() => setOpened(false)}
+            >
+              Contact
+            </Link>
+            <Link
+              to="about"
+              smooth={true}
+              duration={1000}
+              onClick={() => setOpened(false)}
+            >
+              About
+            </Link>
+          </nav>
+        )}
+      </div>
+    </>
+  );
+}
