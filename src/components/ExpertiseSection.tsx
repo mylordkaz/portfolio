c1230ba159372bec99acecd6ad2c26d4eb98ebc4
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-04-06 00:22:56 +0900

    shadow-box color expertise

--- src/components/ExpertiseSection.tsx
+++ src/components/ExpertiseSection.tsx
@@ -3,28 +3,29 @@ import './expertise.css';
 export default function ExpertiseSection() {
   return (
     <>
-      <div className="expertise-container">
+      <div className="expertise">
         <h2>expertise</h2>
+        <div className="container-wrap">
+          <div className="exp-container">
+            <h4>Front-end:</h4>
+            <p>working with JavaScript & TypeScript on the React Framework.</p>
+            <p>Also, CSS with Tailwind or Vanilla & Chakra UI</p>
+            <div className="icon-container"></div>
+          </div>
 
-        <div>
-          <h4>Front-end:</h4>
-          <p>working with JavaScript & TypeScript on the React Framework.</p>
-          <p>Also, CSS with Tailwind or Vanilla & Chakra UI</p>
+          <div className="exp-container">
+            <h4>Back-end:</h4>
+            <p>working with Node.js & Express.js, using also TypeScript.</p>
+            <p>PosgreSQL database with Prisma ORM </p>
+            <div className="icon-container"></div>
+          </div>
+          <div className="exp-container">
+            <h4>Other:</h4>
+            <p>Deployment with Netlify and Render.</p>
+            <p>Authentication & cloud with Firebase.</p>
+            <div className="icon-container"></div>
+          </div>
         </div>
-        <div className="icon-container"></div>
-
-        <div>
-          <h4>Back-end:</h4>
-          <p>working with Node.js & Express.js, using also TypeScript.</p>
-          <p>PosgreSQL database with Prisma ORM </p>
-        </div>
-        <div className="icon-container"></div>
-        <div>
-          <h4>Other:</h4>
-          <p>Deployment with Netlify and Render.</p>
-          <p>Authentication & cloud with Firebase.</p>
-        </div>
-        <div className="icon-container"></div>
       </div>
     </>
   );
