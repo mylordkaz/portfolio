f3396e432f239ae2e4ef7939dd3e454a49d69bf4
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-04-08 22:56:57 +0900

    basic text styling

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -5,18 +5,19 @@ export default function About() {
     <>
       <div className="about">
         <div className="about-title">
-          <h1>About</h1>
+          <h1>About.</h1>
         </div>
 
         <div className="presentation-container">
           <img src={me} alt="" />
           <h2>I'm a Full-Stack Software Engineer based in Japan.</h2>
-          <p>
+          <p className="p1">
             "After gaining valuable skills in management and customer service
-            during my career in hospitality, I've transitioned to software
-            engineering, driven by a passion for technology and its potential
-            impact."
+            during my career in hospitality. <br /> I've transitioned to
+            software engineering, driven by a passion for technology and its
+            potential impact."
           </p>
+          <div className="horizontal-bar"></div>
           <div className="description-container">
             <p>
               "I love learning and embracing new challenges. I'm always seeking
@@ -24,7 +25,7 @@ export default function About() {
               find both interesting and powerful. I have a great interest for
               blockchain and web3 in general."
             </p>
-
+            <div className="horizontal-bar"></div>
             <p>
               "When it comes to my hobbies, <br /> I'm passionate about
               motorsports and mechanics. During my spare time, you'll often find
