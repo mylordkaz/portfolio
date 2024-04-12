b114f2c86b3181014d5c3de260138c0682c3e228
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-04-12 17:28:52 +0900

    add project card div

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -9,34 +9,39 @@ export default function ProjectSection() {
         <h1>Projects</h1>
       </div>
 
-      <div className="project-container">
-        <img src={strive} alt="" />
-        <div className="project-text">
-          <h2>Strive</h2>
+      <div className="project-card">
+        <div className="project-container">
+          <img src={strive} alt="" />
+          <div className="project-text">
+            <h2>Strive</h2>
 
-          <p>
-            Strive is a modern, user-focused personnel management solution that
-            lets you easily manage your most valuable resource: your team.
-          </p>
+            <p>
+              Strive is a modern, user-focused personnel management solution
+              that lets you easily manage your most valuable resource: your
+              team.
+            </p>
+          </div>
+        </div>
+        <div className="btn-container">
+          <button>
+            <a href="https://strive-management.com/">view project</a>
+          </button>
         </div>
       </div>
-      <div className="btn-container">
-        <button>
-          <a href="https://strive-management.com/">view project</a>
-        </button>
-      </div>
-      <div className="project-container">
-        <img src={crypt} alt="" />
-        <div className="project-text">
-          <h2>Cryptrack</h2>
+      <div className="project-card">
+        <div className="project-container">
+          <img src={crypt} alt="" />
+          <div className="project-text">
+            <h2>Cryptrack</h2>
 
-          <p>Enabling users to track their cryptocurrency investments</p>
+            <p>Enabling users to track their cryptocurrency investments</p>
+          </div>
         </div>
-      </div>
-      <div className="btn-container">
-        <button>
-          <a href="https://mycryptrack.netlify.app/">view project</a>
-        </button>
+        <div className="btn-container">
+          <button>
+            <a href="https://mycryptrack.netlify.app/">view project</a>
+          </button>
+        </div>
       </div>
     </>
   );
